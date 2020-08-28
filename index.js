894cc0cc2120a01ab9a347063aa64440493e1aac
junior <trader.junior492047@gmail.com>
2020-08-28 15:47:03 +0700

    ssr web

---
+++ index.js
@@ -0,0 +1,13 @@
+// function sendEmail() {
+// 	Email.send({
+// 	Host: "smtp.gmail.com",
+// 	Username : "trader.junior492047@gmail.com",
+// 	Password : "Junior0990576878",
+// 	To : '<recipient’s email address>',
+// 	From : "<sender’s email address>",
+// 	Subject : "<email subject>",
+// 	Body : "<email body>",
+// 	}).then(
+// 		message => alert("mail sent successfully")
+// 	);
+// }
